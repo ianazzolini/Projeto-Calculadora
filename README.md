8fd5b34e8fd930fa0cd9e8752cb3a3ba403db162
ianazzolini <ian_azzolini@hotmail.com>
2022-09-13 21:57:51 -0300

    Projeto Calculadora

---
+++ README.md
@@ -0,0 +1,2 @@
+# Desenvolvendo uma Mini calculadora
+> Mini calculadora em HTML, CSS E JS.
---
+++ js/scrip.js
@@ -0,0 +1,25 @@
+var resultado = document.querySelector("#resultado")
+
+function calcula(valor){
+    /*alert(`Você digitou ${valor}`)*/
+   resultado.value += valor
+
+    switch (resultado.value[0]){
+        case '*':
+            resultado.value = ''
+        case '/':
+            resultado.value = ''
+        case '.':
+            resultado.value = ''    
+    }
+}
+function limpar(){
+resultado.value = ''
+}
+
+function apagar(){
+    resultado.value = resultado.value.substring(0,(resultado.value.length -1))
+}
+function resultadoFinal(){
+    resultado.value = eval(resultado.value)
+}
